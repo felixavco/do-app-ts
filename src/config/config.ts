13efd9721bfe7ff346a5e616fb19286c362d396e
Felix Avelar <felixavco@gmail.com>
2019-05-29 11:58:22 -0600

    working on send notifications

---
+++ src/config/config.ts
@@ -0,0 +1,12 @@
+export const smtp_config = {
+    host: process.env.SMTP_SERVER,
+	port: process.env.SMTP_PORT,
+	smtp_user: process.env.SMTP_USER,
+	smtp_pwd: process.env.SMTP_PWD
+}
+
+
+export const Imsg_data = {
+	from: "no-rely@doc-app.com",
+}
+
